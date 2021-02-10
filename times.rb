f7ae0befd14c8cdc695a25388a3b501c5a763360
Ksenia serdyukova <kingsbroo@gmail.com>
2021-02-10 01:15:48 -0400

    commit tree 3e30aee6998e3a6eec1fa7095181764902b07a26

--- times.rb
+++ times.rb
@@ -1,7 +1,16 @@
 def using_times
-	#your code here
+
+	7.times do
+		puts "Wingardium Leviosa"
+	end
 end
 
 
 
 
+#Fork and clone this lab.
+#Run the test suite with the learn test command.
+#You'll be coding your solution in times.rb:
+#Fill out the content of the using_times method so that calling it 
+#will puts the desired phrase, "Wingardium Leviosa" seven times by using the times keyword.
+ 
